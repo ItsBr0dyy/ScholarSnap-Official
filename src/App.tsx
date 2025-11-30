c6ca275b64c3198e767263c8f441c70f69a4f1fe
ItsBr0dyy <231164304+ItsBr0dyy@users.noreply.github.com>
2025-11-29 22:08:07 -0800

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <h1>Welcome to ScholarSnap!</h1>
+        <p>This is your AI homework helper.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
